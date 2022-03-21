ddeb6e9423bc45d911b10194e3a4026832f9ae73
Arrow-row <1512803831@qq.com>
2022-03-21 12:43:54 +0800

    0321

--- practices/ch5/SubClass.java
+++ practices/ch5/SubClass.java
@@ -1,8 +1,25 @@
 class BaseClass{
 	public int a=5;
+	public int book=6;
+	public void base(){
+		System.out.println("normal function of parent");
+	}
+	public void test(){
+		System.out.println("overridden function of parent");
+	}
 }
 public class SubClass extends BaseClass{
 	public int a=7;
+
+	//重新定义book实例变量隐藏父类book实例变量
+	public String book="2022";
+	public void test(){
+		System.out.println("subclass function overriding parent's");
+	}
+	public void sub(){
+		System.out.println("normal function of subclass");
+	}
+
 	public void accessOwner(){
 		System.out.println(a);
 	}
@@ -10,9 +27,27 @@ public class SubClass extends BaseClass{
 		System.out.println(super.a);	//super 访问父类的实例变量
 	}
 	public static void main(String[] args){
-		SubClass sc=new SubClass();	
-		sc.accessOwner();
-		sc.accessBase();
+		//SubClass sc=new SubClass();	
+		//sc.accessOwner();
+		//sc.accessBase();
+
+		BaseClass bc=new BaseClass();	//使用BaseClass系统默认无参构造器
+		System.out.println(bc.book);
+		bc.base();
+		bc.test();
+
+		SubClass sc=new SubClass();
+		System.out.println(sc.book);
+		sc.base();
+		sc.test();
+
+		BaseClass ploymophicBc=new SubClass();
+		System.out.println(ploymophicBc.book);	//输出6，表明访问的是BaseClass对象的实例变量
+		ploymophicBc.base();	//执行从父类继承的base()
+		ploymophicBc.test();	//执行当前类test()
+		//ploymophicBc.sub()	//ploymophicBc编译时类型是BaseClass,BaseClass 类未提供sub方法，此行编译时会报错
+
+	
 	}
 }
 

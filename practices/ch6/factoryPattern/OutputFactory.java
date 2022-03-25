ae30215e910940f65c7ad7ff6993a0243350c0ad
Arrow-row <1512803831@qq.com>
2022-03-25 16:38:56 +0800

    factoryPattern

---
+++ practices/ch6/factoryPattern/OutputFactory.java
@@ -0,0 +1,13 @@
+public class OutputFactory{
+	public Output getOutput(){
+		return new Printer();
+		//return new BetterPrinter();
+	}
+	public static void main(String[] args){
+		OutputFactory of=new OutputFactory();
+		Computer c=new Computer(of.getOutput());
+		c.keyIn("string 1...");
+		c.keyIn("string 2...");
+		c.print();
+	}
+}

f5416b7283aebbd58893ad2e5ba58f6b8d036431
Arrow-row <1512803831@qq.com>
2022-06-07 22:36:53 +0800

    review

--- practices/ch5/5.2/OverloadVarargs.java
+++ practices/ch5/5.2/OverloadVarargs.java
@@ -4,7 +4,7 @@ public class OverloadVarargs{
 	public void test(String msg){
 		System.out.println("***func test() that contains only one string***");
 	}
-	public void test(String... books){
+	public void test(String... books){ //1.形参个数可变的参数本质就是一个数组参数,不能通过直接传入一个字符串参数来调用  2.此处的个数可变形参不包含一个字符串参数的形式
 		System.out.println("***func test() that the formal args are changeable***");
 	}
 	public static void main(String[] args){
